5d07505747ecbd044d5b03b1456eb39b49e23fe2
kiakauppi <n2kaki00@students.oamk.fi>
2024-02-06 14:40:45 +0200

    Tehtävään lisätty tekstissä ollut tehtävä

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,9 @@
 let rand_num1 = 0
 let rand_num2 = 0
 
+let oikea_vast = 0
+let vaara_vast = 0
+
 const getRandomIntNumberInRange = (min,max) => {
     return Math.floor(Math.random() * max) + min
 }
@@ -21,11 +24,18 @@ addEventListener("DOMContentLoaded", () =>{
 document.querySelector('button').addEventListener('click', () =>{
     const answer =Number(document.querySelector('input').value)
     const correctAnswer = rand_num1 + rand_num2
+
     if(answer===correctAnswer){
         alert('Correct!')
+        oikea_vast= oikea_vast + 1
+        document.querySelector('#correct').innerHTML = oikea_vast
+        console.log(oikea_vast)
     }
     else{
         alert('Incorrect!')
+        vaara_vast = vaara_vast + 1
+        document.querySelector('#incorrect').innerHTML = vaara_vast
+        console.log(vaara_vast)
     }
 
     randomizeNumbers()
